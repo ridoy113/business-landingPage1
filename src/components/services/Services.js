b7a31b7fc0d915b463220bbe2c113335f430feb6
ridoy113 <almamounhridoy113@gmail.com>
2022-09-20 14:37:57 +0600

    box makes grid

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -6,7 +6,30 @@ import './services.css';
 const Services = () => {
     return (
         <div className='container'>
-            <h1>hEllo</h1>
+            <div className='box'>
+
+                <div>
+                    <h2>01</h2>
+                    <h2>Call Service</h2>
+                    <p>Learn More</p>
+                </div>
+                <div>
+                    <h2>02</h2>
+                    <h2>Your Strategy</h2>
+                    <p>Learn More</p>
+                </div>
+                <div>
+                    <h2>03</h2>
+                    <h2>Team Building</h2>
+                    <p>Learn More</p>
+                </div>
+                <div>
+                    <h2>04</h2>
+                    <h2>Premium plan</h2>
+                    <p>Learn More</p>
+                </div>
+
+            </div>
         </div>
     );
 };
